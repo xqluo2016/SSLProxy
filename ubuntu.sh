0c4e36f844957d86ee69bd9062848c1a9a66ec8a
luoxq <xq.luo@foxmail.com>
2018-07-18 11:35:55 +0800

    Create ubuntu.sh
    Ubuntu init script

---
+++ ubuntu.sh
@@ -0,0 +1,35 @@
+apt-get --assume-yes install apache2
+cd /etc/apache2
+a2enmod ssl proxy proxy_http proxy_connect
+
+mkdir /etc/apache2/ssl
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/ssl/apache.key -out /etc/apache2/ssl/apache.crt <<ENDL
+
+
+
+
+
+
+
+
+
+ENDL
+
+sed -i 's/\/etc\/ssl\/certs\/ssl-cert-snakeoil.pem/\/etc\/apache2\/ssl\/apache.crt/g' /etc/apache2/sites-available/default-ssl.conf
+
+sed -i 's/\/etc\/ssl\/private\/ssl-cert-snakeoil.key/\/etc\/apache2\/ssl\/apache.key/g' /etc/apache2/sites-available/default-ssl.conf
+
+
+sed -i '5iProxyRequests On' /etc/apache2/sites-available/default-ssl.conf
+sed -i '6iProxyVia On' /etc/apache2/sites-available/default-ssl.conf
+sed -i '7i<Proxy "*">' /etc/apache2/sites-available/default-ssl.conf
+sed -i '8i</Proxy>' /etc/apache2/sites-available/default-ssl.conf
+
+
+sed -i 's/Listen 80/#Listen 80/g' /etc/apache2/ports.conf
+
+
+a2ensite default-ssl.conf 
+
+service apache2 restart
