7d60efaa4067cbb8a72e67c7ba45b342e5003f25
luoxq <xq.luo@foxmail.com>
2018-07-23 22:55:20 +0800

    Update windows_chrome.cmd
    IP:PORT

--- windows_chrome.cmd
+++ windows_chrome.cmd
@@ -1,5 +1,5 @@
-set /p IP="Enter IP: "
+set /p IP="Enter IP:PORT : "
 
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --ignore-certificate-errors  --user-data-dir=c:\chrome1 --proxy-server=https://%IP%:443 
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --ignore-certificate-errors  --user-data-dir=c:\chrome1 --proxy-server=https://%IP% 
 
 pause
