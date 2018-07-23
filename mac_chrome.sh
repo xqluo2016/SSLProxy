6de766af2833ca5ffc6ccb3d39366a6e4af83276
luoxq <xq.luo@foxmail.com>
2018-07-23 22:55:52 +0800

    Update mac_chrome.sh
    IP:PORT

--- mac_chrome.sh
+++ mac_chrome.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" == "" ]
 then
-	echo "Please enter your server IP:"
+	echo "Please enter your server IP:PORT :"
 	read IP
 else
 	IP=$1
@@ -10,6 +10,6 @@ fi
 
 cd ~
 mkdir -p .chrome1
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --ignore-certificate-errors  --user-data-dir=$(pwd)/.chrome1 --proxy-server=https://$IP:443 
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --ignore-certificate-errors  --user-data-dir=$(pwd)/.chrome1 --proxy-server=https://$IP 
 
 
